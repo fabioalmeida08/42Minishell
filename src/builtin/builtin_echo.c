4431d1da97c321379401577f7f58a08d84d5e471
Fabio Almeida <fabioalmeida0896@gmail.com>
2026-01-06 15:22:01 -0300

    feat: built echo done with -n option

--- src/builtin/builtin_echo.c
+++ src/builtin/builtin_echo.c
@@ -6,7 +6,7 @@
 /*   By: fabialme <fabialme@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/06 14:50:19 by fabialme          #+#    #+#             */
-/*   Updated: 2026/01/06 15:00:34 by fabialme         ###   ########.fr       */
+/*   Updated: 2026/01/06 15:21:40 by fabialme         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,9 @@ void	builtin_echo(char **cmd, t_shell *sh)
 		printf("%s",cmd[2]);
 		sh->exit_status = 1;
 	}
-	else if (cmd[2])
+	else
 	{
-		printf("%s\n",cmd[2]);
+		printf("%s\n",cmd[1]);
 		sh->exit_status = 1;
 	}
 }
