6f2d940e65e1945024b2ff1f5d6a0682ca5980da
Fabio De Carvalho Almeida <fabialme@c1r1p1.42sp.org.br>
2026-01-27 17:47:49 -0300

    fix: unset with multiples args

--- src/builtin/builtin_unset.c
+++ src/builtin/builtin_unset.c
@@ -6,7 +6,7 @@
 /*   By: bolegari <bolegari@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/10 16:07:42 by bolegari          #+#    #+#             */
-/*   Updated: 2026/01/27 17:46:36 by fabialme         ###   ########.fr       */
+/*   Updated: 2026/01/27 17:47:34 by fabialme         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@ void	builtin_unset(char **cmd, t_shell *sh)
 	int	i;
 
 	sh->exit_status = 0;
-	while (cmd[i])
 	i = 1;
+	while (cmd[i])
 	{
 		if (get_env_node(sh->env_list, cmd[i]))
 			remove_env_var(&sh->env_list, cmd[i]);
